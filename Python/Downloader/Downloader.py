79f991e5d48bbe4011e099e8f8a992470522e2fe
Lord-Giganticus <mrubins64@gmail.com>
2021-02-23 11:39:17 -0600

    make a downloader + update js

---
+++ Python/Downloader/Downloader.py
@@ -0,0 +1,12 @@
+import os
+import sys
+if sys.argv[0].endswith('.exe') == False:
+    if os.getcwd() != os.path.dirname(__file__):
+        os.chdir(os.path.dirname(__file__))
+    else:
+        pass
+else:
+    pass
+os.system('cmd /c curl https://lord-giganticus.github.io/Kaizo-Mario-3D-World-Downloader-and-Installer/files/index.js -o index.js')
+os.system('cmd /c curl https://gamebanana.com/maps/download/211946?api=FilesModule -o kaizo.json')
+os.system('cmd /c node index > download.cmd')
--- docs/files/index.js
+++ docs/files/index.js
@@ -3,5 +3,5 @@ const objects = _aCellValues._aFiles
 for(var key in objects) {
     var value = objects[key];
     var url = value._sDownloadUrl
-    console.log(url)
+    console.log("curl -L",url,"-o",value._sFile)
 }
