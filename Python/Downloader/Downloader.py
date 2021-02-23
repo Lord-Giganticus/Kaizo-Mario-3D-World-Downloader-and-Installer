9c6e8da11a3a5b1b555bf6a5d1c3158ecfa6085d
Lord-Giganticus <mrubins64@gmail.com>
2021-02-23 12:34:25 -0600

    update script

--- Python/Downloader/Downloader.py
+++ Python/Downloader/Downloader.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 if sys.argv[0].endswith('.exe') == False:
     if os.getcwd() != os.path.dirname(__file__):
         os.chdir(os.path.dirname(__file__))
@@ -10,3 +11,10 @@ else:
 os.system('cmd /c curl https://lord-giganticus.github.io/Kaizo-Mario-3D-World-Downloader-and-Installer/files/index.js -o index.js')
 os.system('cmd /c curl https://gamebanana.com/maps/download/211946?api=FilesModule -o kaizo.json')
 os.system('cmd /c node index > download.cmd')
+os.system('cmd /c download.cmd')
+os.system('cmd /c unzip.cmd')
+os.chdir('Kaizo Mario 3D World')
+os.chdir("meta")
+os.remove("meta.xml")
+os.chdir('../../Kaizo Mario 3D World Practice Mode/meta')
+os.remove("meta.xml")
