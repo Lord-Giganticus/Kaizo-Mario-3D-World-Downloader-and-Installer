7d5a722a98ff46498b5e3a741b4feb8bc562605c
Lord-Giganticus <mrubins64@gmail.com>
2021-02-24 14:14:32 -0600

    start installer

---
+++ Installer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Net;
+
+namespace Installer
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
