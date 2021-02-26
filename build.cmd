9c39ca09267efeb495e180bbd3d553d86d80ea78
Lord-Giganticus <mrubins64@gmail.com>
2021-02-26 08:01:51 -0600

    finish build script

--- build.cmd
+++ build.cmd
@@ -1,3 +1,5 @@
 msbuild "Installer\Installer.csproj" -p:Configuration=Release
 cd Installer\bin\Release
 Installer.exe
+mv Kaizo Mario 3D World Normal Mode.exe ../../../.
+mv Kaizo Mario 3D World Practice Mode.exe ../../../.
