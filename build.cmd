f080147cfaea5423133cb668c2f4580d26e08557
Lord-Giganticus <mrubins64@gmail.com>
2021-02-27 11:36:44 -0600

    fix build.cmd

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 msbuild "Installer\Installer.csproj" -p:Configuration=Release
 cd Installer\bin\Release
 Installer.exe
-mv "Kaizo Mario 3D World.exe" ../../../.
 7z a Release.zip "Kaizo Mario 3D World.exe"
+mv Release.zip ../../../.
 echo Complete
