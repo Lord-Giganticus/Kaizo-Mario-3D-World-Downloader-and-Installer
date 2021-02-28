a01c62abe0825aef2d8c8d82a2211e9e40f9f4bc
Lord-Giganticus <mrubins64@gmail.com>
2021-02-28 13:17:12 -0600

    fix build.cmd

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@ msbuild "Installer\Installer.csproj" -p:Configuration=Release
 msbuild "Updater\Updater.csproj" -p:Configuration=Release
 cd Updater
 makensis Updater.nsi
-copy "Kaizo Mario 3D World Updater.exe" "..\Installer\bin\Release\Kaizo Matio 3D World Updater.exe"
+copy "Kaizo Mario 3D World Updater.exe" "..\Installer\bin\Release\Kaizo Mario 3D World Updater.exe"
 cd ..\Installer\bin\Release
 Installer.exe
 7z a Release.zip "Kaizo Mario 3D World.exe"
