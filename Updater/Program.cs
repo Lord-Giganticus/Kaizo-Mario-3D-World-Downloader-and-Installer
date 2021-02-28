bc470f4b7ebfff2219fd7b33bbf1f9d59931dc96
Lord-Giganticus <mrubins64@gmail.com>
2021-02-28 13:26:04 -0600

    update how the config is downloaded

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Net;
 
 namespace Updater
 {
@@ -9,10 +8,7 @@ namespace Updater
     {
         static void Main(string[] args)
         {
-            using (var client = new WebClient())
-            {
-                client.DownloadFile("https://api.github.com/repos/Lord-Giganticus/Kaizo-Mario-3D-World-Downloader-and-Installer/releases/latest", "config.json");
-            }
+            Process.Start("CMD.exe", "/c curl https://api.github.com/repos/Lord-Giganticus/Kaizo-Mario-3D-World-Downloader-and-Installer/releases/latest -o config.json");
             Process.Start("CMD.exe", "node checker 2.04 > download.cmd");
             foreach (string line in File.ReadLines("download.cmd"))
             {
