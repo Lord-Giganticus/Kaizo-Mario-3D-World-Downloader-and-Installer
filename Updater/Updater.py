4072366d9a307767e2ba90fa2f48f55266306928
Lord-Giganticus <mrubins64@gmail.com>
2021-03-01 15:13:46 -0600

    add some things to Updater

--- Updater/Updater.py
+++ Updater/Updater.py
@@ -3,6 +3,7 @@ import os
 import urllib.request
 import json
 import requests
+import time
 
 if sys.argv[0].endswith(".exe") == False:
     if os.getcwd() != os.path.dirname(__file__) == True:
@@ -15,7 +16,7 @@ url = 'https://api.github.com/repos/Lord-Giganticus/Kaizo-Mario-3D-World-Downloa
 with urllib.request.urlopen(url) as f:
     html = f.read().decode('utf-8')
 with open("config.json",'w') as j:
-    j.writelines(html)
+    j.write(html)
     j.close()
 with open('config.json','r') as f:
   data = json.load(f)
@@ -26,9 +27,12 @@ asset = assets[0]
 url = asset['browser_download_url']
 name = asset['name']
 if sys.argv[1] != tag:
+    print("Starting update in 5 seconds. This will take some time depending on your internet data rate!")
+    time.sleep(5)
     r = requests.get(url, allow_redirects=True)
     with open(name, 'wb') as f:
         f.write(r.content)
+        f.close()
 elif sys.argv[1] == tag:
     print("No need to update.")
 else:
