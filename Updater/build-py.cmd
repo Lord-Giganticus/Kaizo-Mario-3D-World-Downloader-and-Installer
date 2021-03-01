2d16d4981a3b9d01387d3c8f15bc11ed566e3cae
Lord-Giganticus <mrubins64@gmail.com>
2021-03-01 07:48:09 -0600

    update scripts

---
+++ Updater/build-py.cmd
@@ -0,0 +1,2 @@
+python -m pip install -r requirements.txt
+pyinstaller -F -i Normal.ico -n "Kaizo Mario 3D World Updater.exe" Updater.py
--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 msbuild "Installer\Installer.csproj" -p:Configuration=Release
-msbuild "Updater\Updater.csproj" -p:Configuration=Release
 cd Updater
+build-py
 makensis Updater.nsi
 copy "Kaizo Mario 3D World Updater.exe" "..\Installer\bin\Release\Kaizo Mario 3D World Updater.exe"
 cd ..\Installer\bin\Release
