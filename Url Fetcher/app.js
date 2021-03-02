7106a03b3937c9e3b8acfbbdf995dd6ae34b5f81
Lord-Giganticus <mrubins64@gmail.com>
2021-03-01 18:45:04 -0600

    fix js cuz GB broke their server cert

--- Url Fetcher/app.js
+++ Url Fetcher/app.js
@@ -5,5 +5,5 @@ const objects = _aCellValues._aFiles
 for (var key in objects) {
     var value = objects[key];
     var url = value._sDownloadUrl
-    console.log("curl -L", url, "-o", value._sFile)
+    console.log("curl -k -L", url, "-o", value._sFile)
 }
