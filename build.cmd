f9369dcd566c348af38dba5e46a986197956729e
Lord-Giganticus <mrubins64@gmail.com>
2021-03-01 19:03:26 -0600

    more scipt updates

--- build.cmd
+++ build.cmd
@@ -10,6 +10,7 @@ makensis Updater.nsi
 copy "Kaizo Mario 3D World Updater.exe" "..\Installer\bin\Release\Kaizo Mario 3D World Updater.exe"
 cd ..\Installer\bin\Release
 Installer.exe
+mv "Kaizo Mario 3D World.exe" ../../../.
+cd ../../../
 7z a Release.zip "Kaizo Mario 3D World.exe"
-mv Release.zip ../../../.
 echo Complete
