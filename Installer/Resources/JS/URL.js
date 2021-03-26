d2507fa533bc39d70d83ce5e03de209792ad15b3
Lord-Giganticus <mrubins64@gmail.com>
2021-03-26 08:31:43 -0600

    update js

--- Installer/Resources/JS/URL.js
+++ Installer/Resources/JS/URL.js
@@ -1,9 +1,17 @@
 // Made by Lord-Giganticus
-
-const { _aCellValues } = require('./config.json');
-const objects = _aCellValues._aFiles
-for (var key in objects) {
-    var value = objects[key];
+const fs = require('fs');
+const data = require('./config.json')
+var files = data._aCellValues._aFiles
+var line = ""
+for (var key in files) {
+    var value = files[key];
     var url = value._sDownloadUrl
-    console.log("curl -k -L", url, "-o", value._sFile)
+    line = line + "curl -k -L "+ url+ " -o "+ value._sFile + "\n" 
 }
+fs.writeFile('download.cmd', line, err => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    return console.log("Complete.");
+})
