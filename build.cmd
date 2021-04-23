198cb982e9e635d0330f900d16a9a0a9e03082b6
Lord-Giganticus <mrubins64@gmail.com>
2021-04-23 16:52:56 -0600

    h

--- build.cmd
+++ build.cmd
@@ -1,21 +1,20 @@
 dotnet clean --configuration Release && dotnet nuget locals all --clear
-dotnet nuget add source https://api.nuget.org/v3/index.json -n nuget.org --configfile $env:APPDATA\NuGet\NuGet.Config
 nuget restore
 cd Updater
 dotnet add package Newtonsoft.Json --version 13.0.1
 msbuild Updater.csproj -p:Configuration=Release
 cd bin/Release/net5.0/publish/win-x86
 cd ../../../../../../
-cp Updater/bin/Release/net5.0/Updater.exe Updater.exe
-cp Updater/bin/Release/net5.0/Updater.dll Updater.dll
-cp Updater/bin/Release/net5.0/Updater.runtimeconfig.json Updater.runtimeconfig.json
-cp Updater/bin/Release/net5.0/Newtonsoft.Json.dll Newtonsoft.Json.dll
+copy Updater\bin\Release\net5.0\Updater.exe .
+copy Updater\bin\Release\net5.0\Updater.dll .
+copy Updater\bin\Release\net5.0\Updater.runtimeconfig.json .
+cp Updater\bin\Release\net5.0\Newtonsoft.Json.dll .
 msbuild Installer/Installer.csproj -p:Configuration=Release
 cd Installer\bin\Release
-mv ../../../Updater.exe Updater.exe
-mv ../../../Updater.dll Updater.dll
-mv ../../../Updater.runtimeconfig.json Updater.runtimeconfig.json
-mv ../../../Newtonsoft.Json.dll Newtonsoft.Json.dll
+mv ../../../Updater.exe .
+mv ../../../Updater.dll .
+mv ../../../Updater.runtimeconfig.json .
+mv ../../../Newtonsoft.Json.dll .
 Installer.exe
 mv "Kaizo Mario 3D World.exe" ../../../.
 cd ../../../
