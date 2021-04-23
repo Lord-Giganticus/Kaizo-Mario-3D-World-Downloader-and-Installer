b6f8d3ffb79a35cd029069ecd4e29fa78be428af
Lord-Giganticus <mrubins64@gmail.com>
2021-04-23 16:07:59 -0600

    make some updates

--- build.cmd
+++ build.cmd
@@ -1,12 +1,18 @@
 nuget restore
 cd Updater
-msbuild Updater.csproj /p:Configuration=Release /p:PublishProfile=win-x86
+msbuild Updater.csproj -p:Configuration=Release
 cd bin/Release/net5.0/publish/win-x86
 cd ../../../../../../
-cp Updater/bin/Release/net5.0/publish/win-x86/Updater.exe Updater.exe
+cp Updater/bin/Release/net5.0/Updater.exe Updater.exe
+cp Updater/bin/Release/net5.0/Updater.dll Updater.dll
+cp Updater/bin/Release/net5.0/Updater.runtimeconfig.json Updater.runtimeconfig.json
+cp Updater/bin/Release/net5.0/Newtonsoft.Json.dll Newtonsoft.Json.dll
 msbuild Installer/Installer.csproj -p:Configuration=Release
 cd Installer\bin\Release
 mv ../../../Updater.exe Updater.exe
+mv ../../../Updater.dll Updater.dll
+mv ../../../Updater.runtimeconfig.json Updater.runtimeconfig.json
+mv ../../../Newtonsoft.Json.dll Newtonsoft.Json.dll
 Installer.exe
 mv "Kaizo Mario 3D World.exe" ../../../.
 cd ../../../
