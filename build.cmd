b0cafb9a69d93b2dff852a8ae288df8d72cd5045
Lord-Giganticus <mrubins64@gmail.com>
2021-04-25 14:12:23 -0600

    potential fix??

--- build.cmd
+++ build.cmd
@@ -1,11 +1,13 @@
+pushd %CD%
 dotnet clean --configuration Release && dotnet nuget locals all --clear
 nuget restore
 cd Updater
 dotnet publish /p:Configuration=Release /p:PublishProfile=win-x86
-cd "bin\Release\net5.0\publish\win-x86"
+cd ../
+cd Updater\bin\Release\net5.0\publish\win-x86
 echo %CD%
 makensis Updater.nsi
-cd ../../../../../../
+popd
 copy Updater\bin\Release\net5.0\publish\win-x86\KM3DW-Updater.exe .
 msbuild Installer/Installer.csproj -p:Configuration=Release
 echo %CD%
