45439e825f7a4dd3a4db72cb94f21f28aec44cbc
Lord-Giganticus <mrubins64@gmail.com>
2021-04-25 13:55:54 -0600

    h

--- build.cmd
+++ build.cmd
@@ -3,12 +3,12 @@ nuget restore
 cd Updater
 dotnet publish /p:Configuration=Release /p:PublishProfile=win-x86
 cd "bin\Release\net5.0\publish\win-x86"
+echo %CD%
 makensis Updater.nsi
 cd ../../../../../../
 copy Updater\bin\Release\net5.0\publish\win-x86\KM3DW-Updater.exe .
 msbuild Installer/Installer.csproj -p:Configuration=Release
 echo %CD%
-pause
 cd Installer\bin\Release
 mv ../../../KM3DW-Updater.exe .
 Installer.exe
