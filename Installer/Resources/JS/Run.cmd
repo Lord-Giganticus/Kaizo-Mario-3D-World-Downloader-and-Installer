d7b7b7dc06da36d6c9379f95609d320389ecb44e
Lord-Giganticus <mrubins64@gmail.com>
2021-05-20 10:36:24 -0600

    Broke run.cmd lol

--- Installer/Resources/JS/Run.cmd
+++ Installer/Resources/JS/Run.cmd
@@ -1,2 +1,2 @@
-curl -k -L https://gamebanana.com/mods/download/149492 -o kaizo_mario_3d_world_269.zip
-curl -k -L https://gamebanana.com/mods/download/149492 -o kaizo_mario_3d_world_practice_269.zip
+curl -k -L https://gamebanana.com/dl/543972 -o kaizo_mario_3d_world_269.zip
+curl -k -L https://gamebanana.com/dl/543971 -o kaizo_mario_3d_world_practice_269.zip
