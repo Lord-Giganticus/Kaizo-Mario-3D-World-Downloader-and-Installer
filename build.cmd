8cdc940c8a302a0bfff9696b585bde60acebeabb
Lord-Giganticus <mrubins64@gmail.com>
2021-05-31 07:25:36 -0600

    fix script

--- build.cmd
+++ build.cmd
@@ -8,11 +8,11 @@ cd Updater\bin\Release\net5.0\publish\win-x86
 echo %CD%
 makensis Updater.nsi
 popd
-copy Updater\bin\Release\net5.0\publish\win-x86\KM3DW-Updater.exe .
+copy "Updater\bin\Release\net5.0\publish\win-x86\Kaizo Mario 3D World Updater.exe" .
 msbuild Installer/Installer.csproj -p:Configuration=Release
 echo %CD%
 cd Installer\bin\Release
-mv ../../../KM3DW-Updater.exe .
+mv "../../../Kaizo Mario 3D World Updater.exe" .
 Installer.exe
 mv "Kaizo Mario 3D World.exe" ../../../.
 cd ../../../
